9121fa4f9edfc6490b504cc2124a4a33fed64750
Miguel Sánchez Ramírez <miguelschz1997@gmail.com>
2024-07-02 10:31:43 +0200

    Añadida dependencia para usar Spring Data JPA. Refactorizado Modelo de datos para usar JPA

--- taco-cloud/src/main/java/tacos/data/IngredientRepository.java
+++ taco-cloud/src/main/java/tacos/data/IngredientRepository.java
@@ -1,15 +1,8 @@
 package tacos.data;
 
+import org.springframework.data.repository.CrudRepository;
 import tacos.model.Ingredient;
 
-import java.util.Optional;
-
-public interface IngredientRepository {
-
-    Iterable<Ingredient> findAll();
-
-    Optional<Ingredient> findById(String id);
-
-    Ingredient save(Ingredient ingredient);
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
 }
--- taco-cloud/src/main/java/tacos/model/TacoOrder.java
+++ taco-cloud/src/main/java/tacos/model/TacoOrder.java
@@ -5,17 +5,22 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.annotation.Id;
 
 @Data
+@Entity
 public class TacoOrder implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@NotBlank(message = "Delivery name is required")
@@ -34,7 +39,8 @@ public class TacoOrder implements Serializable {
 	private String ccExpiration;
 	@Digits(integer = 3, fraction = 0, message = "Invalid CVV")
 	private String ccCVV;
-	
+
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<Taco> tacos = new ArrayList<>();
 
 	private Date placedAt;
