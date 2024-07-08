9d8ce94abc9b3f14bae7103e970afc6aa68cb81d
Miguel Sánchez Ramírez <miguelschz1997@gmail.com>
2024-07-08 11:25:27 +0200

    Duplicado el proyecto taco-cloud para configurarlo con Cassandra NoSQL

---
+++ taco-cloud-cassandra/src/main/java/tacos/utils/IngredientByIdConverter.java
@@ -0,0 +1,29 @@
+package tacos.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import tacos.data.IngredientRepository;
+import tacos.model.Ingredient;
+import tacos.model.Ingredient.Type;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private IngredientRepository ingredientRepository;
+
+    @Autowired
+    public IngredientByIdConverter(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @Override
+    public Ingredient convert(String id) {
+        return ingredientRepository.findById(id).orElse(null);
+    }
+
+}
