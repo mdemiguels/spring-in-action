1d08a38adf29b4f7efc61fe7d349a299839e1ebd
Miguel Sánchez Ramírez <miguelschz1997@gmail.com>
2024-07-11 12:47:50 +0200

    Recreado el proyecto para usar con mongo. En este ejemplo solo se dockeriza el mongo y mongo-express

---
+++ taco-cloud-mongo/src/main/java/tacos/data/OrderRepository.java
@@ -0,0 +1,10 @@
+package tacos.data;
+
+import org.springframework.data.repository.CrudRepository;
+import tacos.model.TacoOrder;
+
+import java.util.UUID;
+
+public interface OrderRepository extends CrudRepository<TacoOrder, String> {
+
+}
